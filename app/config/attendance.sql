52f408722a5dc8394172ef53017f34813f1a738c
Amed-Dev <intiprda@gmail.com>
2024-01-16 00:36:35 -0500

    update sql to create the database

--- app/config/attendance.sql
+++ app/config/attendance.sql
@@ -1,14 +1,24 @@
 -- crear base de datos
+CREATE DATABASE attendance;
 
 -- crear la tabla trabajador
 CREATE TABLE trabajador(
   ID_TRABAJADOR INT AUTO_INCREMENT NOT NULL,
   NOMBRE VARCHAR(150) NOT NULL,
-  SECTOR_TRABAJO  VARCHAR(150) NOT NULL, 
-  ASISTIO BIT NOT NULL,
-  FALTA BIT NOT NULL,
-  TARDANZA BIT NOT NULL,
+  SECTOR_TRABAJO  VARCHAR(150) NOT NULL,
+  CARGO VARCHAR(70) NOT NULL,
+  ID_ASISTENCIA INT NOT NULL,
   PRIMARY KEY (ID_TRABAJADOR)
+  FOREIGN KEY (ID_ASISTENCIA) REFERENCES asistencia_estado(ID)
 );
--- seleccionar los campos de la base de datos
+-- seleccionar los campos de la tabla trabajador
 SELECT * FROM trabajador;
+
+-- crear tabla asistencia_estado
+CREATE TABLE asistencia_estado (
+  ID INT AUTO_INCREMENT NOT NULL,
+  ESTADO VARCHAR(30) NOT NULL,
+  PRIMARY KEY (ID)
+);
+-- seleccionar campos de la tabla asistencia_estado
+SELECT * FROM asistencia_estado;
